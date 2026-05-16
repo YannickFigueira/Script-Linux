238c673826a5bfcdcea7bb8baa65793fa0f3cac7
YannickFigueira <chronostimeinchain@gmail.com>
2026-05-16 13:15:34 -0300

    criado configuração raid

---
+++ Ubuntu Server/raid_config.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+identificar_discos_raid() {
+    # Lista apenas discos físicos, ignora partições, loops, rom e o disco do sistema (sda)
+    DISCOS_RAID=$(lsblk -d -n -o NAME,TYPE | grep 'disk' | awk '{print $1}' | grep -v 'sda')
+
+    if [ -z "$DISCOS_RAID" ]; then
+        echo "Erro: Nenhum disco secundário livre foi encontrado para o RAID."
+    else
+        echo "=== Discos Detectados para o RAID ==="
+        for disco in $DISCOS_RAID; do
+            echo "-> /dev/$disco"
+        done
+        echo "-------------------------------------"
+    fi
+}
